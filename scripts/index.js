5fb80b0bdb00d6b00ba13f12e9129df4872f83ca
Andrew.Worley <drewjw1212@gmail.com>
2024-04-19 18:04:48 -0400

    project 4

--- scripts/index.js
+++ scripts/index.js
@@ -24,11 +24,44 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-
-/*         Elements           */
+/*-----------------------------*/
+/*         Elements            */
+/*-----------------------------*/
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profileEditModal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+/*-----------------------------*/
+/*          Functions          */
+/*-----------------------------*/
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
 
+/*-----------------------------*/
+/*        Event Handlers       */
+/*-----------------------------*/
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/*-----------------------------*/
+/*        Event Listeners      */
+/*-----------------------------*/
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
